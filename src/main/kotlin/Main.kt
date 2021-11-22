e6c0101317fe19da8fc4d2a7379d48cc7e36d309
JesusVasari <jlopez 2rafaelalberti.es>
2021-11-22 21:03:37 +0100

    commit 8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -57,7 +57,7 @@ class Modulo(var numeroDeAlumnos: Int = 15, maxAlumnos: Int = 20) {
     fun numeroAprobados(evaluacion: String): Int {
 
         var evaluacion = notas[evaluacion.toInt()]
-        var aprobados = evaluacion.count {it >=5.0f}
+        var aprobados = evaluacion.count { it >= 5.0f }
         return (aprobados)
     }
 
@@ -85,37 +85,61 @@ class Modulo(var numeroDeAlumnos: Int = 15, maxAlumnos: Int = 20) {
         var aprobados = evaluacion.count { it == 10.0f }
         return true
     }
+
     fun hayAlumnosAprobados(evaluacion: String): Boolean {
         var evaluacion = notas[evaluacion.toInt()]
-        var aprobados = evaluacion.count {it ==5.0f}
+        var aprobados = evaluacion.count { it == 5.0f }
         return true
     }
+
     fun primeraNotaNoAprobada(evaluacion: String): Float {
         var evaluacion = notas[evaluacion.toInt()]
-        var aprobados = evaluacion.first {it ==5.0f}
+        var aprobados = evaluacion.first { it == 5.0f }
         return aprobados
     }
-    fun listaNotasOrdenados(evaluacion: String): List<Pair<String,Float>> {
 
-        var ordenado =notas.sort()
-        return (matriculado)
-    }
+    /*
+        fun listaNotasOrdenados(evaluacion: String): List<Pair<String, Float>> {
+
+            var ordenado = notas.sort()
+      //      return (matriculado)
+        }
+    */
     fun matricularAlumno(alumno: Alumno): Boolean {
 
-        matriculados[0]=Alumno()
-     var i = 0
-        while ((i < matriculados.size - 1) && (matriculados[i] !=null)){
-        i ++}
-        return false
+        var pos = 0
+        var matriculado = false
+
+        //Buscar la posicion en la que insertar el alumno
+        while ((pos < matriculados.size - 1) && (matriculados[pos] != null)) {
+            pos++
+        }
+
+        if (pos <matriculados.size -1) {
+
+            matriculados.set(pos,alumno)
+        }
+
+        return matriculado
     }
 
     fun bajaAlumno(idAlumno: String): Boolean {
 
-        matriculados[0]=Alumno()
-        var i = 0
-        while ((i < matriculados.size - 1) && (matriculados[i] !=null)){
-            i --}
-        return false
+
+        var pos = 15
+        var matriculado = false
+
+        //Buscar la posicion en la que insertar el alumno
+        while ((pos < matriculados.size - 1) && (matriculados[pos] != null)) {
+            pos--
+        }
+
+        if (pos <matriculados.size -1) {
+
+           matriculados.set(pos, Alumno?)
+        }
+
+        return matriculado
     }
 
 
@@ -128,45 +152,34 @@ data class Alumno(val id: String = "", val nombre: String = " ", var ap1: String
 
 
 fun main(args: Array<String>) {
-
-
-    var alumno1 = Alumno("0", "Juan", "Cuesta", "Gil")
-    var alumno2 = Alumno("1", "Belén", "López", "Vazquez")
-    var alumno3 = Alumno("2", "Vicenta", "Benito", "Gil")
-    var alumno4 = Alumno("3", "Marisa", "Benito", "Navarro")
-    var alumno5 = Alumno("4", "Natalia", "Cuesta", "Hurtado")
-    var alumno6 = Alumno("5", "Fernando", "Tejero", "Delgado")
-    var alumno7 = Alumno("6", "Andrés", "Guerra", "Romero")
-    var alumno8 = Alumno("7", "Isabel", "Ruíz", "Ordaz")
-    var alumno9 = Alumno("8", "Roberto", "Alonso", "Castillo")
-    var alumno10 = Alumno("9", "Fernando", "Navarro", "Collado")
-
-
-
     var m: Modulo = Modulo(15)
-    m.establecerNota("0", Modulo.EV_PRI, 6.0f )
-    m.establecerNota("1", Modulo.EV_PRI,  7.3f)
-    m.establecerNota("2", Modulo.EV_PRI,  5.7f)
-    m.establecerNota("3", Modulo.EV_PRI,  10.0f)
-    m.establecerNota("4", Modulo.EV_PRI,  8.56F)
-    m.establecerNota("5", Modulo.EV_PRI,  6.4f)
+    val lista = listOf(
+        Alumno("0", "Juan", "Cuesta", "Gil"),
+        Alumno("1", "Belén", "López", "Vazquez"),
+        Alumno("2", "Vicenta", "Benito", "Gil"),
+        Alumno("3", "Marisa", "Benito", "Navarro"),
+        Alumno("4", "Natalia", "Cuesta", "Hurtado"),
+        Alumno("5", "Fernando", "Tejero", "Delgado"),
+        Alumno("6", "Andrés", "Guerra", "Romero"),
+        Alumno("7", "Isabel", "Ruíz", "Ordaz"),
+        Alumno("8", "Roberto", "Alonso", "Castillo"),
+        Alumno("9", "Fernando", "Navarro", "Collado")
+    ).forEach { m.matricularAlumno(it) }
+
+
+
+
+    m.establecerNota("0", Modulo.EV_PRI, 6.0f)
+    m.establecerNota("1", Modulo.EV_PRI, 7.3f)
+    m.establecerNota("2", Modulo.EV_PRI, 5.7f)
+    m.establecerNota("3", Modulo.EV_PRI, 10.0f)
+    m.establecerNota("4", Modulo.EV_PRI, 8.56F)
+    m.establecerNota("5", Modulo.EV_PRI, 6.4f)
     m.establecerNota("6", Modulo.EV_PRI, 4.9F)
-    m.establecerNota("7", Modulo.EV_PRI,  5.7F)
-    m.establecerNota("8", Modulo.EV_PRI,  4.8F)
-    m.establecerNota("9", Modulo.EV_PRI,  5.8F)
+    m.establecerNota("7", Modulo.EV_PRI, 5.7F)
+    m.establecerNota("8", Modulo.EV_PRI, 4.8F)
+    m.establecerNota("9", Modulo.EV_PRI, 5.8F)
 
 
-
-    m.matricularAlumno(alumno10)
-    m.matricularAlumno(alumno1)
-    m.matricularAlumno(alumno2)
-    m.matricularAlumno(alumno3)
-    m.matricularAlumno(alumno4)
-    m.matricularAlumno(alumno5)
-    m.matricularAlumno(alumno6)
-    m.matricularAlumno(alumno7)
-    m.matricularAlumno(alumno8)
-    m.matricularAlumno(alumno9)
-
-    m.notaMasAlta()
+  m.notaMedia()
 }
